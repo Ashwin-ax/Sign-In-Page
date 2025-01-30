57f93f260d9492277d4a3eba64d8ba30d118b7bf
Ashwin Suryawanshi <ashwinpatil394@gmail.com>
2025-01-30 18:10:46 +0530

    Update signin credentials.sql
    This SQL file stores the username and password of the user. When the Python file is running, the username and password existing in this student table will then give the result as login successful.

--- signin credentials.sql
+++ signin credentials.sql
@@ -1,13 +1,10 @@
 CREATE DATABASE students;
 USE students;
+
 CREATE TABLE student(
 	username VARCHAR(50),
     passwords VARCHAR(50)
 );
-
-ALTER TABLE 
-student RENAME COLUMN password to passwords;
-
 
 INSERT INTO student
 (username, passwords)
